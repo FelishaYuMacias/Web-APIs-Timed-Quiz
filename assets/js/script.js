0167580f2ddbb758f37e1fa8c64344e25b993cde
Felisha <felisha.j.yu@gmail.com>
2022-10-03 15:01:47 -0700

    added pseudo code and linked start page to correct button on highscorepage html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,25 @@
 console.log("test")
+
+//TODO:Create timed quiz that stores high scores
+
+// write quiz questions-reference questions.js
+
+
+//TODO: WHEN I click the start button
+        // THEN a timer starts and I am presented with a question
+            //create a function to start the game
+            //start timer
+             //hide questions then reveal first and hide start when start is selected
+
+//TODO: WHEN I answer a question
+        //THEN I am presented with another question
+        //reveal subsequent qustions when answer is selected 
+
+//TODO: WHEN I answer a question incorrectly
+        //THEN time is subtracted from the clock
+
+//TODO: WHEN all questions are answered or the timer reaches 0
+        //THEN the game is over
+
+//TODO: WHEN the game is over
+        //THEN I can save my initials and my score
