afb6dfcd125bd304eb3844e147c43c3c51caf54f
Felisha <felisha.j.yu@gmail.com>
2022-10-03 09:01:33 -0700

    moving questions to js file instead of html

---
+++ assets/js/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        question: "Is The Velocipastor the greatest movie of all time?"
+        options: ["No", "Yes","IDK", "Hell Yes"]
+        right: "Hell Yes"
+    },
+    {
+        question: "Where does the Velocipastor get his powers?"
+        options: ["Idaho", "China", "New York", "Australia"]
+        right: "China"
+    },
+    {
+        question: "Who or what does the Velocipastor fight using his powers?"
+        options: ["Other Dinosaurs", "Crime", "Ninjas", "Crime and Ninjas"]
+        right: "Crime and Ninjas"
+    },
+    {
+        question: "How are his parents killed?"
+        options: ["Car fire", "Car crash", "Murderd by ninjas", "He eats them" ]
+        right: "Car fire"
+    },
+    {
+        question: "What was the estimtated budget for the film?"
+        options: ["$32,000", "$59,000", "$78,000", "$103,000"]
+        right: " "
+    },
+]
