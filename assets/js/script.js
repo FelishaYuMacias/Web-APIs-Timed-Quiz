b1a7e6a89487670aca648f5097a6db28fe2e2a7f
Felisha <felisha.j.yu@gmail.com>
2022-10-03 17:41:53 -0700

    put question and anwers options on page after start is pressed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,62 @@
 var timerEl = document.querySelector("#timer");
 var startBtn = document.querySelector("#start");
 var startPage = document.querySelector("#start-page");
+var quizPage = document.querySelector ("#quiz")
+var quizQuestion = document.querySelector ("#questions")
+var quizOptions = document.querySelector ("#options")
+var quizCorrect = document.querySelector ("right")
 
-var secondsLeft = 10;
+var secondsLeft = questions.length*5;
+var index = 0
 //TODO:Create timed quiz that stores high scores
 
 // write quiz questions-reference questions.js
 
 
 //TODO: WHEN I click the start button
-    //create a function to start the game
-    function gameStart() {
-        //hide start screen
-        startPage.setAttribute("class", "hide");
-
-        // THEN a timer starts 
-            // Sets interval in variable
-            var timerInterval = setInterval( function() {
-              secondsLeft--;
-              timerEl.textContent = secondsLeft + " Seconds until you lose!";
-          
-              if(secondsLeft === 0) {
-                // Stops execution of action at set interval
-                clearInterval(timerInterval);
-                // ends game at 0 seconds
-                // endGame;
-                alert("No Points for You!");
-              }
-           // and I am presented with a question
-            
-             //hide questions then reveal first and hide start when start is selected
-              function loadQuestion() {
-
-              }
-
-            }, 1000);
-            
-
-
-        }  
+//create a function to start the game
+function gameStart() {
+    //hide start screen
+    startPage.setAttribute("class", "hide");
+    //show question page
+    quizPage.setAttribute ("class", "show")
     
-    startBtn.addEventListener("click", gameStart)
+    // THEN a timer starts 
+    // Sets interval in variable
+    var timerInterval = setInterval( function() {
+        secondsLeft--;
+        timerEl.textContent = secondsLeft + " Seconds until you lose!";
+        
+        if(secondsLeft === 0) {
+            // Stops execution of action at set interval
+            clearInterval(timerInterval);
+            // ends game at 0 seconds
+            // endGame;
+            alert("No Points for You!");
+        }
+        // and I am presented with a question
+        
+        //hide questions then reveal first and hide start when start is selected
+        
+    }, 1000);
+    loadQuestion() 
+} 
+
+//choose question and options from array
+function loadQuestion(index) {
+   
+    //put question and answers on screen
+    quizQuestion.textContent= currentQuestion.question;
+
+    //create button for each answer
+    quizOptions.textContent= currentQuestion.options
     
+    quizOptions.setAttribute("class","answers")
+            }
+        
+
+startBtn.addEventListener("click", gameStart)
+        
        
 
 //TODO: WHEN I answer a question
